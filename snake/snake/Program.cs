62e68d9e45f903b87d1be614662e5777f5abb2af
babilonsuxx <babilonsuxx@gmail.com>
2015-09-25 22:55:29 +0300

    инкапсуляция и конструкторы

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,6 +10,14 @@ namespace snake
     {
         static void Main(string[] args)
         {
+            Point p1 = new Point(1,3,'*');
+            
+            p1.Draw();
+
+            Point p2 = new Point(4,5,'#');
+            
+            p2.Draw();
+            Console.ReadLine();
         }
     }
 }
